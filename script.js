908cabeca9e7d8ff167fbc7112f0684f82feb765
CS-Aditya-Rawat <adityainferno4812@gmail.com>
2021-01-05 20:45:17 +0530

    Add joke and time

--- script.js
+++ script.js
@@ -15,11 +15,32 @@ const videoSearch = (key, search, maxResults) => {
 			});
 		};
 
+		
+const currTime = () => {
+	let hours = addZero(today.getHours());
+	let minutes = addZero(today.getMinutes());
+	let seconds = addZero(today.getSeconds());
+
+	let curr_time = `${hours}:${minutes}:${seconds}`;
+}
+
+function addZero(num){
+	return num < 10 ? `0${num}`:num;
+}
+
 const weatherData = (key, cityName) => {
 	$.get("api.openweathermap.org/data/2.5/weather?q=" + cityName +"&appid="+ key, function(data){
 		console.log(data);
 	})
 };
+
+const giveWeather = (Main, text,temp,disc,icon) => {
+	Main.innerHtml += `
+	<div class="row">
+        <div class="chat answer">${text}<br><b>${temp}</b><br><b>${disc}</b><img src="${icon}"></div>
+    </div>
+	`
+}
 
 const addAnswer = (Main, text) =>
 {
@@ -70,34 +91,70 @@ recognition.onresult = (e) => {
 	console.log(words);
 	addQuestion(Main, transcript);
 
+	//--------------------Greeting
 	if(transcript == 'hello'){
 		const utter = new SpeechSynthesisUtterance("Hi, How are you");
 		synth.speak(utter);
 		addAnswer(Main, utter.text);
 	} 
-	else if(
-		words.forEach((word)=>{
-			word === 'video';
-		})
-	){
-		console.log(word);
-		// $(document).ready(function(){
-		// 	const API_KEY = "AIzaSyA7qnrGy4J8JaB6Op-2ib1la5_MaUwVqUU"
+
+	//----------------------Youtube Videos
+	else if(words.includes('play' && 'video'))
+	{
+		let utter = new SpeechSynthesisUtterance("Result From Youtube are :")
+		//Show Youtube videos
+		stringWithCommas = (words.slice(2)).toString();
+		var stringWithoutCommas = stringWithCommas.replace(/,/g, '+');
+		console.log(stringWithoutCommas);
+		const API_KEY = "AIzaSyA7qnrGy4J8JaB6Op-2ib1la5_MaUwVqUU";
+		synth.speak(utter);
+		addAnswer(Main, "For more click below", videoSearch(API_KEY, stringWithoutCommas, 10));
+
+		//URL for search on youtube main Page
+		let url = "https://www.youtube.com/results?search_query="+stringWithoutCommas;
+
+		let utter2 = new SpeechSynthesisUtterance("For more click me ")
+		synth.speak(utter2);
 		
-		// 	var video = '';
-		// 	var videos = $("#videos");
-		// 	$("#form").submit(function (event) {
-		// 		event.preventDefault();
-		// 		var search = $("#search").val()
-		// 		videoSearch(API_KEY, search, 100);
-		// 	});
-		// });
+		
 		
 	}
-	// else if()
-	// {
-	// 	const api_key = "668e95bfc9414269499a7ba975a1f2db"
-	// }
+
+	// --------------------WEATHER API
+	else if(words.includes('weather' && 'at'))
+	{
+		cityName = words[2];
+		function weatherBalloon( cityName ) {
+			var key = '668e95bfc9414269499a7ba975a1f2db';
+			fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName+'&units=imperial&appid=' + key)  
+			.then(function(resp) { return resp.json() }) // Convert data to json
+			.then(function(data) {
+			  let temp = data.main.temp;
+			  let disc = data.weather[0].main;
+			  let icon = data.weather[0].icon;
+			  console.log(temp)
+			  console.log(disc)
+			  giveWeather(Main, 'The temperature is', temp, disc, icon);
+			})
+			.catch(function() {
+			  // catch any errors
+			});
+		  }
+		  weatherBalloon(cityName);
+	}
+	else if(words.includes('joke'))
+	{
+		async function getJoke(){
+			const jokedata = await fetch('https://icanhazdadjoke.com/', {
+				headers: {
+					'Accept': 'application/json'
+				}
+			});
+			const jokeObj = await jokedata.json();
+			console.log(jokeObj.joke);
+		}
+		getJoke();
+	}
 };
 
 D.addEventListener("DOMContentLoaded",()=>{
